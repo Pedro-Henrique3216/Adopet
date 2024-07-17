db6c4c0b823d620e60b1854c3ea01e1da33bf2a1
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-07-16 21:54:24 -0300

    feat: criado endereco exception

---
+++ src/main/java/br/com/alura/challenge/adopet/exception/EnderecoException.java
@@ -0,0 +1,8 @@
+package br.com.alura.challenge.adopet.exception;
+
+public class EnderecoException extends RuntimeException {
+
+    public EnderecoException(String message) {
+        super(message);
+    }
+}
