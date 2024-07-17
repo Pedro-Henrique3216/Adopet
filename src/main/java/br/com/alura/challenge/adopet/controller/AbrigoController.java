3961642dbb7ee592b1650d147c8df30c292c5a2a
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-07-17 12:58:13 -0300

    feat: criado metodo para retornar uma pagina com todos os abrigos

--- src/main/java/br/com/alura/challenge/adopet/controller/AbrigoController.java
+++ src/main/java/br/com/alura/challenge/adopet/controller/AbrigoController.java
@@ -2,19 +2,21 @@ package br.com.alura.challenge.adopet.controller;
 
 import br.com.alura.challenge.adopet.dto.CadastroAbrigo;
 import br.com.alura.challenge.adopet.dto.DadosDetalhamentoAbrigo;
+import br.com.alura.challenge.adopet.dto.DadosRetornoAbrigo;
 import br.com.alura.challenge.adopet.model.Abrigo;
 import br.com.alura.challenge.adopet.service.AbrigoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+
 
 @RestController
 @RequestMapping("/abrigos")
@@ -29,6 +31,11 @@ public class AbrigoController {
         Abrigo abrigo = service.cadastrarAbrigo(dto);
         URI uri = uriComponentsBuilder.path("/abrigos/{id}").buildAndExpand(abrigo.getId()).toUri();
         return ResponseEntity.created(uri).body(new DadosDetalhamentoAbrigo(abrigo));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosRetornoAbrigo>> listarAbrigos(Pageable pageable) {
+        return ResponseEntity.ok(service.listarAbrigos(pageable));
     }
 
 }
--- src/main/java/br/com/alura/challenge/adopet/service/AbrigoService.java
+++ src/main/java/br/com/alura/challenge/adopet/service/AbrigoService.java
@@ -2,11 +2,15 @@ package br.com.alura.challenge.adopet.service;
 
 import br.com.alura.challenge.adopet.dto.CadastroAbrigo;
 import br.com.alura.challenge.adopet.dto.DadosDetalhamentoAbrigo;
+import br.com.alura.challenge.adopet.dto.DadosRetornoAbrigo;
 import br.com.alura.challenge.adopet.exception.EnderecoException;
 import br.com.alura.challenge.adopet.model.Abrigo;
 import br.com.alura.challenge.adopet.model.Endereco;
 import br.com.alura.challenge.adopet.repository.AbrigoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -30,5 +34,15 @@ public class AbrigoService {
         cep = cep.replace("-", "");
         String uri = "http://viacep.com.br/ws/" + cep + "/json/";
         return new RestTemplate().getForObject(uri, Endereco.class);
+    }
+
+    public Page<DadosRetornoAbrigo> listarAbrigos(Pageable pageable) {
+        Page<Abrigo> abrigos = repository.findAll(pageable);
+        if(abrigos.isEmpty()){
+            throw new EntityNotFoundException("Não existe um abrigo cadastrado");
+        }
+        return abrigos
+                .map(DadosRetornoAbrigo::new);
+
     }
 }
