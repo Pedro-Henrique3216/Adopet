b5f04ba46114ef2c84fca6f39161f9d7594bb90c
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-07-16 21:54:41 -0300

    feat: tratando erro de busca de endereço

--- src/main/java/br/com/alura/challenge/adopet/infra/TratarErro.java
+++ src/main/java/br/com/alura/challenge/adopet/infra/TratarErro.java
@@ -1,5 +1,6 @@
 package br.com.alura.challenge.adopet.infra;
 
+import br.com.alura.challenge.adopet.exception.EnderecoException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,6 +11,11 @@ public class TratarErro {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> tratarErroDeTutorNaoEncontrado(EntityNotFoundException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(EnderecoException.class)
+    public ResponseEntity<String> tratarErroDeBuscaDeEndereco(EnderecoException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
