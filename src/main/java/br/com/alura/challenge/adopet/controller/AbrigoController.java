c8417b4529eea974baba5860d69845dd53548f7c
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-07-16 21:54:09 -0300

    feat: criado metodo de cadastro de abrigo com busca de endereco pelo cep

---
+++ src/main/java/br/com/alura/challenge/adopet/controller/AbrigoController.java
@@ -0,0 +1,26 @@
+package br.com.alura.challenge.adopet.controller;
+
+import br.com.alura.challenge.adopet.dto.CadastroAbrigo;
+import br.com.alura.challenge.adopet.model.Abrigo;
+import br.com.alura.challenge.adopet.service.AbrigoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/abrigos")
+public class AbrigoController {
+
+    @Autowired
+    private AbrigoService service;
+
+    @PostMapping
+    @Transactional
+    public Abrigo adicionaAbrigo(@RequestBody CadastroAbrigo dto) {
+        return service.cadastrarAbrigo(dto);
+    }
+
+}
