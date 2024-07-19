44da634f6d6cf5315ccedb34ed5fa811bf1da3e3
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-07-19 09:41:01 -0300

    refactor: colocando anotação no detalhamento de pet

--- src/main/java/br/com/alura/challenge/adopet/dto/pet/DadosDetalhamentoPet.java
+++ src/main/java/br/com/alura/challenge/adopet/dto/pet/DadosDetalhamentoPet.java
@@ -1,6 +1,10 @@
 package br.com.alura.challenge.adopet.dto.pet;
 
 import br.com.alura.challenge.adopet.model.Pet;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.UUID;
 
@@ -17,7 +21,7 @@ public record DadosDetalhamentoPet(
         String idade,
 
         String imagem,
-
+        @JsonProperty("abrigo_id")
         UUID abrigoId
 ) {
 
