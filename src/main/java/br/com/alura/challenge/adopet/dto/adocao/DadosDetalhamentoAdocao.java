0330a0f6259983c76d0fb129f26eeb6b53be3169
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-07-22 18:55:06 -0300

    refactor: alterando dto para mostrar a data da adoção

--- src/main/java/br/com/alura/challenge/adopet/dto/adocao/DadosDetalhamentoAdocao.java
+++ src/main/java/br/com/alura/challenge/adopet/dto/adocao/DadosDetalhamentoAdocao.java
@@ -3,15 +3,20 @@ package br.com.alura.challenge.adopet.dto.adocao;
 import br.com.alura.challenge.adopet.model.Abrigo;
 import br.com.alura.challenge.adopet.model.Adocao;
 import br.com.alura.challenge.adopet.model.Endereco;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 public record DadosDetalhamentoAdocao(
         UUID id,
         UUID animal,
-        UUID tutor
+        UUID tutor,
+        String data
 ) {
     public DadosDetalhamentoAdocao(Adocao adocao){
-        this(adocao.getId(), adocao.getPet().getId(), adocao.getTutor().getId());
+        this(adocao.getId(), adocao.getPet().getId(), adocao.getTutor().getId(), adocao.getDate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
     }
 }
