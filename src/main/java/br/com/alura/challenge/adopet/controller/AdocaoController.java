ba538516a4ee172b61b8516a7c3ce47d8fea298a
Pedro-Henrique3216 <ph6704938@gmail.com>
2024-07-22 19:02:18 -0300

    refactor: adicionando o response entity

--- src/main/java/br/com/alura/challenge/adopet/controller/AdocaoController.java
+++ src/main/java/br/com/alura/challenge/adopet/controller/AdocaoController.java
@@ -4,6 +4,7 @@ import br.com.alura.challenge.adopet.dto.adocao.CadastroAdocao;
 import br.com.alura.challenge.adopet.dto.adocao.DadosDetalhamentoAdocao;
 import br.com.alura.challenge.adopet.service.AdocaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +20,9 @@ public class AdocaoController {
 
     @PostMapping
     @Transactional
-    public DadosDetalhamentoAdocao adotar(@RequestBody CadastroAdocao dto) {
-        return service.createAdocao(dto);
+    public ResponseEntity<DadosDetalhamentoAdocao> adotar(@RequestBody CadastroAdocao dto) {
+        return ResponseEntity.ok(service.createAdocao(dto));
     }
+
+
 }
